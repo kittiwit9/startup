e51d5102c4ffd85d77b12d7fdfd507e11232bdc9
Kittiwit <kittiwit@Kittiwits-MacBook-Pro.local>
2017-05-31 16:02:25 +0700

    update charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
 
 //----------------material
 import {
-  MdButtonModule, 
+  MdButtonModule,
   MdCheckboxModule,
-  MdGridListModule
+  MdGridListModule,
+  MdToolbarModule
 } from '@angular/material';
 
 //-----------------highcharts
@@ -17,19 +22,35 @@ import { ChartModule } from 'angular2-highcharts';
 import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
 import * as highcharts from 'highcharts';
 
+import { HomeComponent } from './home/home.component';
+import { Test1Component } from './test1/test1.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'test1', component: Test1Component },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Test1Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
-    MdButtonModule, 
+    MdButtonModule,
     MdCheckboxModule,
     MdGridListModule,
-    ChartModule
+    ChartModule,
+    MdToolbarModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     { provide: HighchartsStatic, useValue: highcharts }
